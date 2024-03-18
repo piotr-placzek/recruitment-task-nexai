54bc54e3d696167064fdd2f90836b0873431cafe
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-03-18 11:13:26 +0100

    Update TrackingModule

--- api/src/features/tracking/tracking.controller.ts
+++ api/src/features/tracking/tracking.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CarPositionDto } from 'src/shared/dtos/car-position.dto';
+import { RequestValidationError } from 'src/shared/dtos/request-validation-error.dto';
+import { GetCarPositionQueryDto } from './queries/dtos/get-car-position-query.dto';
 import { GetCarPositionQuery } from './queries/get-car-position.query';
 
 @ApiTags('tracking')
@@ -15,8 +17,15 @@ export class TrackingController {
     description: 'Current address, where selected car is',
     type: CarPositionDto,
   })
+  @ApiResponse({
+    status: 400,
+    description: 'Validation error',
+    type: RequestValidationError,
+  })
   @ApiResponse({ status: 500, description: 'Internal server error' })
-  async getCarPosition(@Param('carId') carId: string): Promise<CarPositionDto> {
-    return this.queryBus.execute(new GetCarPositionQuery(carId));
+  async getCarPosition(
+    @Query('dto') dto: GetCarPositionQueryDto,
+  ): Promise<CarPositionDto> {
+    return this.queryBus.execute(new GetCarPositionQuery(dto.id));
   }
 }
--- api/src/features/tracking/tracking.module.ts
+++ api/src/features/tracking/tracking.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import CoreModule from 'src/core/core.module';
+import { CoreModule } from 'src/core/core.module';
 import { TrackingPositionEntity } from 'src/database/tracking-position.entity';
 import { GetCarPositionQueryHandler } from './queries/handlers/get-car-position-query.handler';
 import { TrackingController } from './tracking.controller';
