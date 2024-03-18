540f67f19dfd0e587d4c35e9a703715a75146203
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-03-18 11:17:05 +0100

    Add ApiResponse for /health route

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get('health')
+  @ApiResponse({ status: 200, description: "Always returns 'OK' string" })
   health(): string {
     return 'OK';
   }
