20bdc278985638fcbc6ca9391f876c4870638523
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-03-18 11:20:09 +0100

    Add some class validators to dtos

--- api/src/shared/dtos/address.dto.ts
+++ api/src/shared/dtos/address.dto.ts
@@ -1,10 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPostalCode, IsString, Min } from 'class-validator';
 
 export class AddressDto {
   @ApiProperty()
+  @IsString()
   streetName: string;
+
   @ApiProperty()
+  @IsNumber()
+  @Min(1)
   streetNumber: number;
+
   @ApiProperty()
+  @IsPostalCode()
   zipCode: string;
 }
--- api/src/shared/dtos/customer-details.dto.ts
+++ api/src/shared/dtos/customer-details.dto.ts
@@ -1,22 +1,37 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { AddressDto } from './address.dto';
+import {
+  IsAlpha,
+  IsEmail,
+  IsInstance,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 
 export class CustomerDetailsDto {
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsUUID()
   id: string;
 
   @ApiProperty()
+  @IsAlpha()
   firstName: string;
 
   @ApiProperty()
+  @IsAlpha()
   lastName: string;
 
   @ApiProperty()
+  @IsString()
   companyName: string;
 
   @ApiProperty()
+  @IsEmail()
   email: string;
 
   @ApiProperty()
+  @IsInstance(AddressDto)
   address: AddressDto;
 }
